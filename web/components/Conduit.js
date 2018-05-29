6d1f89798cfc31a8c912ee26a541d121f827b4d0
Anthony Garvan <anthony.garvan@gmail.com>
2018-05-29 00:43:14 -0500

    adding state as array of objects

--- web/components/Conduit.js
+++ web/components/Conduit.js
@@ -6,17 +6,26 @@ console.log(ContentEditable);
 class Conduit extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {topic: "Topic", note: "Notes here"};
+    this.state = {notes: [{note: "Notes here"}, {note: "and here"}, {note:"and here"}]};
     this.handleNoteChange = this.handleNoteChange.bind(this);
   }
 
   handleNoteChange(e) {
-    this.setState({ note: e.target.value });
+    const notes = e.target.value.split('<div>').filter(n => n).map(n => ({note: n.replace('</div>', '')}));
+    this.setState({ notes: notes});
   }
 
   render() {
     return <div>
-    <ContentEditable className="sp-note" html={this.state.note} onChange={this.handleNoteChange} /></div>
+          <div className="sp-header level">
+            <a>
+              <i className="fas fa-search"></i>
+            </a>
+          </div>
+          <ContentEditable
+            className="sp-note"
+            html={this.state.notes.map(n => `<div>${n.note}</div>`).join('')}
+            onChange={this.handleNoteChange} /></div>
   }
 }
 
