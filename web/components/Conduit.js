b70dbc076f046a740a3f3a75d20bd2856c380c92
Anthony Garvan <anthony.garvan@gmail.com>
2018-05-31 16:34:26 -0500

    saving with zangodb

--- web/components/Conduit.js
+++ web/components/Conduit.js
@@ -1,31 +1,72 @@
 const React = require('react');
 const ContentEditable = require('react-contenteditable');
-
-console.log(ContentEditable);
+const zango = require('zangodb');
+const uuid = require('uuid/v1')
 
 class Conduit extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {notes: [{note: "Notes here"}, {note: "and here"}, {note:"and here"}]};
+    document.execCommand('defaultParagraphSeparator', false, 'div');
+    const rootDb = new zango.Db('notes', {notes: ['position', 'note']})
+    this.db = rootDb.collection('notes');
+    console.log(this.db.find({}).sort({ position: -1 }));
+    this.state = {mode: 'note', notes: [], startPosition: 0, endPosition: 1};
+    this.db.find({}).sort({ position: 1 }).toArray((err, notes) => {
+        this.setState({ notes });
+    })
     this.handleNoteChange = this.handleNoteChange.bind(this);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
   }
 
   handleNoteChange(e) {
-    const notes = e.target.value.split('<div>').filter(n => n).map(n => ({note: n.replace('</div>', '')}));
-    this.setState({ notes: notes});
+    console.log(e.target.value);
+    const notesRaw = e.target.value.split('</div>').filter(n => n).map(n => n.replace('<div>', ''));
+    const notes = notesRaw.map((rawNote, i) => {
+      return {note: rawNote, position: this.state.startPosition + (i / notesRaw.length)*(this.state.endPosition - this.state.startPosition)}
+    });
+    this.db.remove({ position:  {$gte: this.state.startPosition, $lte: this.state.endPosition}});
+    this.db.insert(notes);
+    this.setState({ notes });
+  }
+
+  handleSearchChange(e) {
+    const searchTerms = e.target.value.split(' ').filter(t => t).map(t => t.toLowerCase());
+    this.state.notes.filter(note => note.toLowerCase().indexOf())
   }
 
   render() {
-    return <div>
-          <div className="sp-header level">
-            <a>
-              <i className="fas fa-search"></i>
+    return this.state.mode === 'note' ?
+          <div><div className="sp-note-header">
+            <a onClick={() => this.setState({ mode: 'search' })}>
+              <img src="/img/search.png" alt="search" />
             </a>
           </div>
           <ContentEditable
             className="sp-note"
             html={this.state.notes.map(n => `<div>${n.note}</div>`).join('')}
             onChange={this.handleNoteChange} /></div>
+            :
+            <div><div className="sp-search-header">
+            <a onClick={() => this.setState({ mode: 'note' })}><img src="/img/back.png" alt="back" /></a>
+
+            <div className="sp-search field has-addons has-addons-right">
+              <div className="sp-search-box control">
+                <input className="input" type="text"
+                  placeholder="Search your note..."
+                  onChange={this.handleSearchChange}/>
+                <span className="icon is-right">
+                  <a onClick={() => alert('clicked!')}></a>
+                </span>
+              </div>
+              <p class="control">
+                 <a class="button is-transparent">
+                   <img src="/img/remove.png" alt="remove" />
+                 </a>
+               </p>
+            </div>
+            </div>
+            <div className="sp-search-results">results</div>
+            </div>
   }
 }
 
--- web/components/Master.js
+++ web/components/Master.js
@@ -24,22 +24,8 @@ function Master(props) {
 
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="icon" href="/img/favicon.png" />
-          <style dangerouslySetInnerHTML = {{ __html: `@font-face {
-            font-family: 'Raleway';
-            font-style: normal;
-            font-weight: 400;
-            src: local('Raleway'), local('Raleway-Regular'), url(${raleway}) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-          }
+          <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet" />
 
-          @font-face {
-            font-family: 'Pacifico';
-            font-style: normal;
-            font-weight: 400;
-            src: local('Pacifico Regular'), local('Pacifico-Regular'), url(${pacifico}) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-          }`}}>
-          </style>
           <script dangerouslySetInnerHTML={{__html: `
               var template = document.createElement('template');
               template.innerHTML = '<link rel="stylesheet" href="/css/master.css" />';
