46e4e2179b6a3ee2ccde212aaaa5a7fce636c42d
Anthony Garvan <anthony.garvan@gmail.com>
2018-08-24 21:42:38 -0500

    performance improvement and css fix

--- web/components/Bignote.js
+++ web/components/Bignote.js
@@ -346,6 +346,18 @@ class Bignote extends React.Component {
   }
 
   searchNote() {
+    if(!this.props.searchString.trim()) {
+      $('#sp-search-results').html($('#sp-note-content h3').toArray().map(el => el.outerHTML).join('\n'));
+      $(window).scrollTop(0);
+      let that = this;
+      $('#sp-search-results h3').click(function() {
+        that.currentBigNote.selectedBlockId = this.id;
+        that.props.handleToNoteMode();
+      })
+      this.props.searchDone();
+      return;
+    }
+
     const exactMatchRegex = /^"(.+)"$/
     let searchRegex;
     if(exactMatchRegex.test(this.props.searchString)) {
@@ -404,14 +416,6 @@ class Bignote extends React.Component {
     if(searchResults.length === 0) {
       $('#sp-search-results').html('<em>No results.</em>');
       $(window).scrollTop(0);
-    } else if(!this.props.searchString.trim()) {
-      $('#sp-search-results').html($('#sp-note-content h3').toArray().map(el => el.outerHTML).join('\n'));
-      $(window).scrollTop(0);
-      let that = this;
-      $('#sp-search-results h3').click(function() {
-        that.currentBigNote.selectedBlockId = this.id;
-        that.props.handleToNoteMode();
-      })
     } else {
       let html = searchResults.map(el => el.outerHTML).join('\n');
       $('#sp-search-results').html(html);
