f3e03863a3e48b0fd8768cf87ffe4a9e532db402
Anthony Garvan <anthony.garvan@gmail.com>
2018-09-29 21:27:59 -0500

    opening footer links in new window

--- web/App.js
+++ web/App.js
@@ -240,7 +240,7 @@ class App extends React.Component {
                 <button className="modal-close is-large" onClick={() => this.setState({ gettingHelp: false })}></button>
               </div>
               <p>Copyright © 2018. Made with ♥ by <a href="https://www.twitter.com/anthonygarvan">@anthonygarvan</a>. Design by Ryan Thurlwell.</p>
-              <p><a href="/privacy.txt">Privacy</a> | <a href="/terms.txt">Terms</a> | <a href="https://github.com/anthonygarvan/bosslog">Source</a> | <a onClick={() => this.setState({ promptUser: true })}>Pricing</a> | <a onClick={() => this.setState({ gettingHelp: true })}>Cheat sheet</a> | <a onClick={() => this.handleExport()}>Export</a>{ this.state.isAuthenticated &&   <span> | <a onClick={this.handleLogout}>Logout</a></span> }</p>
+              <p><a href="/privacy.txt" target="_blank">Privacy</a> | <a href="/terms.txt" target="_blank">Terms</a> | <a href="https://github.com/anthonygarvan/bosslog" target="_blank">Source</a> | <a onClick={() => this.setState({ promptUser: true })}>Pricing</a> | <a onClick={() => this.setState({ gettingHelp: true })}>Cheat sheet</a> | <a onClick={() => this.handleExport()}>Export</a>{ this.state.isAuthenticated &&   <span> | <a onClick={this.handleLogout}>Logout</a></span> }</p>
               <p>Questions, comments or problems? Feel free to tweet me or file an issue on <a href="https://github.com/anthonygarvan/bosslog/issues">github</a>.</p>
               <div className="sp-logo">
                 <img src="/img/logo.png" alt="logo" />
